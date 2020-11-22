dca57b2338cf2d958c35eed52e8f44b68c5f4f7b
Mark Dyuzhev <projectprakticum@gmail.com>
2020-11-22 21:21:38 +0200

    application has been changed

--- code.c
+++ code.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 #include "header.h"
+#include "header2.h"
 
 int main(void)
 {
-    printf("%d", MAX);
+    printf("Максимальное число мест в аудитории: %d\n", MAX);
+    printf("Число студентов: %d\n", STUDENT_COUNT);
+    printf("Число свободных мест: %d\n", MAX - STUDENT_COUNT);
 
     return 0;
 }
